d3332408049ea6e922e9a739ac44d61efd6b5d9b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-05-31 13:54:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,71 +7,72 @@ var activeTetromino = "stick"
 
 //make an array for each tetromino
 //position 4 is the top middle square
-var cubeArray = [4,5,14,15];
-var stickArray = [4,14,24,34]
+var cubeArray = [4, 5, 14, 15];
+var stickArray = [4, 14, 24, 34]
 
 
 function makeButtons() {
   for (var i = 0; i < 200; i++) {
-      var btn = document.createElement("button");
-      btn.setAttribute("type", "button");
-      btn.setAttribute("class", "btnClass");
-      btn.setAttribute("id", i);
-      document.getElementById("theGrid").appendChild(btn);
+    var btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("class", "btnClass");
+    btn.setAttribute("id", i);
+    document.getElementById("theGrid").appendChild(btn);
   }//end For loop
   makeStick()
 }//end function makeButton 
 
-function makeCube(){
-  for(i=0 ;i<4; i++){
-    document.getElementById(cubeArray[i]).style.backgroundColor = "yellow"  
-  }  
-}
-function makeStick(){
-  for(i=0 ;i<4; i++){
-    document.getElementById(stickArray[i]).style.backgroundColor = "blue"  
-  }  
-}
-function isKeyDown(e){
-  if(e.code == "ArrowLeft"){
-    if(activeTetromino == "stick"){
-      moveLeftStick();
-    }else if (moveLeftStick == "cube"){
-      moveLeftCube();
-    }else if (moveLeftStick == "el"){
-      moveLeftEl();
-    }else if (moveLeftStick == "reverseEl"){
-      moveLeftReverseEl();
-    }else if (moveLeftStick == "es"){
-      moveLeftEs();
-    }else if (moveLeftStick == "reverseEs"){
-      moveLeftReverseEs();
-    }else if (moveLeftStick == "tee"){
-      moveLeftTee();
+function makeCube() {
+  for (i = 0; i < 4; i++) {
+    document.getElementById(cubeArray[i]).style.backgroundColor = "yellow"
   }
 }
-function moveLeftStick(){
-  for(i=0; i <4; i++){
-      document.getElementById(stickArray[i]).style.backgroundColor = "black"
-      stickArray[i] -= 1;
-      document.getElementById(stickArray[i]).style.backgroundColor = "blue"
+function makeStick() {
+  for (i = 0; i < 4; i++) {
+    document.getElementById(stickArray[i]).style.backgroundColor = "blue"
+  }
+}
+function isKeyDown(e) {
+  if (e.code == "ArrowLeft") {
+    if (activeTetromino == "stick") {
+      moveLeftStick();
+    } else if (moveLeftStick == "cube") {
+      moveLeftCube();
+    } else if (moveLeftStick == "el") {
+      moveLeftEl();
+    } else if (moveLeftStick == "reverseEl") {
+      moveLeftReverseEl();
+    } else if (moveLeftStick == "es") {
+      moveLeftEs();
+    } else if (moveLeftStick == "reverseEs") {
+      moveLeftReverseEs();
+    } else if (moveLeftStick == "tee") {
+      moveLeftTee();
     }
+  }
 }
-function moveLeftCube(){
-  
+function moveLeftStick() {
+  for (i = 0; i < 4; i++) {
+    document.getElementById(stickArray[i]).style.backgroundColor = "black"
+    stickArray[i] -= 1;
+    document.getElementById(stickArray[i]).style.backgroundColor = "blue"
+  }
 }
-function moveLeftEl(){
-  
+function moveLeftCube() {
+
 }
-function moveLeftReverseEl(){
-  
+function moveLeftEl() {
+
 }
-function moveLeftEs(){
-  
+function moveLeftReverseEl() {
+
 }
-function moveLeftReverseEs(){
-  
+function moveLeftEs() {
+
 }
-function moveLeftTee(){
-  
+function moveLeftReverseEs() {
+
+}
+function moveLeftTee() {
+
 }
